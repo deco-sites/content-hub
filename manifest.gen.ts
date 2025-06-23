9167b5c39817abc3e63ce9e7dfa7509795b3e546
Paulo Marsiglia <pmarsiglia93@gmail.com>
2025-06-23 15:59:05 -0300

    feature(artigo-padrao-lp): implements artigo padrao lp

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -8,17 +8,19 @@ import * as $$$$$$$$$$$2 from "./apps/deco/vtex.ts";
 import * as $$$$$$$$$$$3 from "./apps/site.ts";
 import * as $$$0 from "./loaders/customVTEX/productById.ts";
 import * as $$$$$$0 from "./sections/Logos.tsx";
-import * as $$$$$$1 from "./sections/Lps/ArticleSliderSection.tsx";
-import * as $$$$$$2 from "./sections/Lps/BannerMediaSection.tsx";
-import * as $$$$$$3 from "./sections/Lps/BannerSliderSection.tsx";
-import * as $$$$$$4 from "./sections/Lps/CategoriesSection.tsx";
-import * as $$$$$$5 from "./sections/Lps/ImageWithTextColumnsSection.tsx";
-import * as $$$$$$6 from "./sections/Lps/InfoCardSection.tsx";
-import * as $$$$$$7 from "./sections/Lps/ReviewsSliderSection.tsx";
-import * as $$$$$$8 from "./sections/Lps/TextSection.tsx";
-import * as $$$$$$9 from "./sections/Product/ProductShelfSection.tsx";
-import * as $$$$$$10 from "./sections/Product/ProductShelfWithComparatorSection.tsx";
-import * as $$$$$$11 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$1 from "./sections/Lps/ArticleGridSection.tsx";
+import * as $$$$$$2 from "./sections/Lps/ArticleSliderSection.tsx";
+import * as $$$$$$3 from "./sections/Lps/BannerMediaSection.tsx";
+import * as $$$$$$4 from "./sections/Lps/BannerSliderSection.tsx";
+import * as $$$$$$5 from "./sections/Lps/CategoriesSection.tsx";
+import * as $$$$$$6 from "./sections/Lps/ImageGridSection.tsx";
+import * as $$$$$$7 from "./sections/Lps/ImageWithTextColumnsSection.tsx";
+import * as $$$$$$8 from "./sections/Lps/InfoCardSection.tsx";
+import * as $$$$$$9 from "./sections/Lps/ReviewsSliderSection.tsx";
+import * as $$$$$$10 from "./sections/Lps/TextSection.tsx";
+import * as $$$$$$11 from "./sections/Product/ProductShelfSection.tsx";
+import * as $$$$$$12 from "./sections/Product/ProductShelfWithComparatorSection.tsx";
+import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -26,17 +28,19 @@ const manifest = {
   },
   "sections": {
     "site/sections/Logos.tsx": $$$$$$0,
-    "site/sections/Lps/ArticleSliderSection.tsx": $$$$$$1,
-    "site/sections/Lps/BannerMediaSection.tsx": $$$$$$2,
-    "site/sections/Lps/BannerSliderSection.tsx": $$$$$$3,
-    "site/sections/Lps/CategoriesSection.tsx": $$$$$$4,
-    "site/sections/Lps/ImageWithTextColumnsSection.tsx": $$$$$$5,
-    "site/sections/Lps/InfoCardSection.tsx": $$$$$$6,
-    "site/sections/Lps/ReviewsSliderSection.tsx": $$$$$$7,
-    "site/sections/Lps/TextSection.tsx": $$$$$$8,
-    "site/sections/Product/ProductShelfSection.tsx": $$$$$$9,
-    "site/sections/Product/ProductShelfWithComparatorSection.tsx": $$$$$$10,
-    "site/sections/Theme/Theme.tsx": $$$$$$11,
+    "site/sections/Lps/ArticleGridSection.tsx": $$$$$$1,
+    "site/sections/Lps/ArticleSliderSection.tsx": $$$$$$2,
+    "site/sections/Lps/BannerMediaSection.tsx": $$$$$$3,
+    "site/sections/Lps/BannerSliderSection.tsx": $$$$$$4,
+    "site/sections/Lps/CategoriesSection.tsx": $$$$$$5,
+    "site/sections/Lps/ImageGridSection.tsx": $$$$$$6,
+    "site/sections/Lps/ImageWithTextColumnsSection.tsx": $$$$$$7,
+    "site/sections/Lps/InfoCardSection.tsx": $$$$$$8,
+    "site/sections/Lps/ReviewsSliderSection.tsx": $$$$$$9,
+    "site/sections/Lps/TextSection.tsx": $$$$$$10,
+    "site/sections/Product/ProductShelfSection.tsx": $$$$$$11,
+    "site/sections/Product/ProductShelfWithComparatorSection.tsx": $$$$$$12,
+    "site/sections/Theme/Theme.tsx": $$$$$$13,
   },
   "apps": {
     "site/apps/deco/blog.ts": $$$$$$$$$$$0,

780dc6f690627de673ae640b6db0c4409e62bcd3
BernardoMarinhoQualityDigital <bernardo.marinho@qualitydigital.global>
2025-07-20 21:28:08 -0300

    fix: Adding a row gap to the ImageCollectionGridSection

--- sections/Lps/ImageCollectionGridSection.tsx
+++ sections/Lps/ImageCollectionGridSection.tsx
@@ -34,13 +34,13 @@ export default function ImageGridSection(
           ))}
         </div>
       ) : imageCollection.length === 2 ? (
-        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-2 justify-center items-center gap-x-[8px]">
+        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-2 justify-center items-center gap-y-[8px] gap-x-[8px]">
           {imageCollection.map((image, index) => (
             <ResponsiveImage src={image.src} key={index} />
           ))}
         </div>
       ) : imageCollection.length === 3 ? (
-        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-3 justify-center items-center gap-x-[8px]">
+        <div className="w-full grid sm:grid-cols-1 lg:grid-cols-3 justify-center items-center gap-y-[8px] gap-x-[8px]">
           {imageCollection.map((image, index) => (
             <ResponsiveImage src={image.src} key={index} />
           ))}

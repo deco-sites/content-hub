3626415aa36b0446ee27c54f185b965b054f8efd
bernardo-marinho_elux <bernardo.marinho@electrolux.com>
2025-08-28 18:48:36 -0300

    feat: finishing FoldItemIsland.

--- islands/FoldItemsIsland.tsx
+++ islands/FoldItemsIsland.tsx
@@ -4,8 +4,8 @@ import ResponsiveImage from "site/components/ui/ResponsiveImage.tsx";
 import type { IResponsiveImage } from "site/types/ResponsiveImage.d.ts";
 
 /**
- * @title Seção de items "abre e fecha".
- * @description Exibe um item ou uma coleção de items que abrem e fecham a partir do click ou toque na imagem.
+ * @title Item "abre e fecha".
+ * @description Um item de uma coleção de um ou mais items.
  */
 export interface FoldItem {
   /**
@@ -60,6 +60,7 @@ function FoldItem({ foldItem }: FoldItem) {
           {title}
         </span>
       </div>
+
       <div
         class={`${isOpen.value ? "h-full" : "hidden"
           } overflow-hidden flex flex-col items-start gap-[8px] py-[16px]`}
@@ -74,19 +75,88 @@ function FoldItem({ foldItem }: FoldItem) {
 }
 
 export default function FoldItemsIsland({ foldItems }: FoldItemsIslandProps) {
+  const selectedItem = useSignal<number>(0);
+
   return (
     <div
-      class={`w-full flex flex-col gap-y-[8px]`}
+      class={`
+        foldItems-container
+        w-full flex flex-col
+        lg:grid lg:grid-cols-[auto_1fr] lg:grid-rows-[1fr] lg:gap-x-[32px]
+      `}
     >
-      {foldItems.length
-        ? (
-          foldItems.map((item, index) => (
-            <FoldItem foldItem={item} key={index} />
-          ))
-        )
-        : (
-          "FoldItemsSection: não há itens a serem renderizados."
-        )}
+      <div class="w-full flex flex-col gap-y-[8px] lg:hidden">
+        {foldItems.map((item, index) => (
+          <FoldItem foldItem={item} key={index} />
+        ))}
+      </div>
+
+      <div
+        class={`
+          hidden relative
+          lg:flex lg:flex-row lg:items-center lg:justify-center lg:h-full lg:gap-x-[8px]
+        `}
+      >
+        {foldItems.map((item, index) => (
+          <div
+            key={index}
+            class={`
+              relative cursor-pointer
+              transition-all duration-300 ease-in-out
+              ${index === selectedItem.value ? 'w-[380px] h-full' : 'w-[120px] h-[486px]'}
+            `}
+            onClick={() => selectedItem.value = index}
+          >
+            <ResponsiveImage
+              {...item.image}
+              class={`h-full w-full object-cover`}
+              link={{}}
+            />
+            <div class="absolute inset-0 bg-black opacity-40"></div>
+            <span
+              class={`
+                absolute bottom-4 
+                ${index === selectedItem.value ? 'left-[388px]' : 'left-[132px]'}
+                text-white font-semibold text-[26px]
+                -rotate-90 transform origin-bottom-left whitespace-nowrap pb-4 pl-4
+              `}
+            >
+              {item.title}
+            </span>
+          </div>
+        ))}
+      </div>
+
+      <div class="hidden lg:flex flex-col justify-center items-start gap-[16px] pr-8">
+        <h2 class="text-[#041E50] font-bold text-4xl">
+          {foldItems[selectedItem.value].title}
+        </h2>
+        <p class="text-[#2B2936] text-base">
+          {foldItems[selectedItem.value].text}
+        </p>
+      </div>
+      <style>
+        {`
+        @media screen and (min-width: 1280px) {
+          .foldItems-container {
+            max-width: 1200px;
+            height: 540px;
+          }
+        }
+        
+        @media screen and (min-width: 1440px) {
+          .foldItems-container {
+            max-width: 1360px;
+          }
+        }
+        
+        @media screen and (min-width: 1920px) {
+          .foldItems-container {
+            max-width: 1600px;
+          }
+        }
+      `}
+      </style>
     </div>
-  )
+  );
 }

b8cb1daf1b647afb63c62635441afdc1461c45a9
Gabriel Carvalho <gabriel.carvalho@qualitydigital.global>
2025-11-03 18:21:12 -0300

    feat(section): add AmbassadorsBannerSliderSection component

---
+++ sections/Lps/AmbassadorsBannerSliderSection.tsx
@@ -0,0 +1,79 @@
+import BannerSlider from "site/islands/BannerSlider.tsx";
+import Section from "site/components/ui/Section.tsx";
+import { useId } from "site/sdk/useId.ts";
+import type { IResponsiveImage } from "site/types/ResponsiveImage.d.ts";
+import type { ISection } from "site/types/Section.d.ts";
+import type { ISliderConfigs } from "site/types/Slider.d.ts";
+import { DefaultBannerSection } from "site/configs/BannerSliderSection.ts";
+
+/**
+ * @description Seção com um slider de banners.
+ */
+interface Props {
+  /**
+   * @title Configuração da Seção
+   * @description Define o título, subtítulo e espaçamento da seção.
+   */
+  section?: ISection;
+
+  /**
+   * @title Banners
+   */
+  banners?: IResponsiveImage[];
+
+  /**
+   * @title Configurações do Slider
+   */
+  configs?: ISliderConfigs;
+}
+
+export default function AmbassadorsBannerSliderSection({
+  section,
+  banners = DefaultBannerSection.banners,
+  configs,
+}: Props) {
+  const id = useId();
+
+  const { slidesPerViewResponsive } = configs ?? {};
+
+  const sliderConfig = {
+    ...configs,
+    slidesPerView: slidesPerViewResponsive?.mobile ?? 1,
+    breakpoints: {
+      768: {
+        slidesPerView: slidesPerViewResponsive?.tablet ?? 1,
+      },
+      1024: {
+        slidesPerView: slidesPerViewResponsive?.desktop ?? 1,
+      },
+    },
+  } as ISliderConfigs;
+
+  if (!banners?.length) return null;
+
+  const defaultPropsBanners = banners.map((banner) => {
+    return {
+      ...{ ...banner },
+      sizes: {
+        ...banner.sizes,
+        fullScreen: true,
+        heightMobile: 400,
+        widthMobile: 375,
+      },
+    };
+  });
+
+  return (
+    <Section
+      {...section}
+      id={id}
+      classesContainer="ambassadors-banner-slider-section"
+    >
+      <BannerSlider
+        configs={sliderConfig}
+        rootId={id}
+        banners={defaultPropsBanners}
+      />
+    </Section>
+  );
+}

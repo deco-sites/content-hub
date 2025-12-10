ebb2457b5636247b6a5b1e2ccd3639e11f1cfaf3
Gabriel Carvalho <gabriel.carvalho@qualitydigital.global>
2025-12-10 17:57:05 -0300

    fix(articles): replace fixed height with min-height utility

--- sections/Lps/ArticlesWithSidebarSection.tsx
+++ sections/Lps/ArticlesWithSidebarSection.tsx
@@ -209,11 +209,11 @@ export default function ArticlesWithSidebarSection({
                         title={article.title ?? ""}
                         classes={{
                           container:
-                            "font-electrolux font-semibold text-[#041E50] text-[24px] leading-none mb-2 px-4 line-clamp-3 h-[72px]",
+                            "font-electrolux font-semibold text-[#041E50] text-[24px] leading-none mb-2 px-4 line-clamp-3 min-h-[72px]",
                         }}
                       />
 
-                      <p class="text-base text-[#4F4F4F] px-4 leading-[1.4] line-clamp-3 h-[67px]">
+                      <p class="text-base text-[#4F4F4F] px-4 leading-[1.4] line-clamp-3 min-h-[67px]">
                         {article.description}
                       </p>
 
